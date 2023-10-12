d941d49c57a7a03edd12b1e50d9bf2663fddb5bc
TristanAly <tristan.aly@gmail.com>
2023-10-12 12:42:15 +0200

    make a test for icons path hash value

--- src/components/Icon/index.test.js
+++ src/components/Icon/index.test.js
@@ -3,16 +3,44 @@ import md5 from "md5";
 import Icon from ".";
 
 describe("Icon component", () => {
-    describe("When a icon is created with name twitch", () => {
-        it("the icon contain this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
-            render(<Icon name="twitch" />)
-            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('327fbc38c8e878259c3ec35ef231517a')
-        });
+  describe("When a icon is created with name twitch", () => {
+    it("the icon contain this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
+      render(<Icon name="twitch" />);
+      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+        "327fbc38c8e878259c3ec35ef231517a"
+      );
     });
-    describe("When a icon is created with name facebook", () => {
-        it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
-            // to complete
-        });
+  });
+  describe("When a icon is created with name facebook", () => {
+    it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
+      render(<Icon name="facebook" />);
+      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+        "bbea4c9e40773b969fdb6e406059f853"
+      );
     });
-})
-
+  });
+  describe("When a icon is created with name twitter", () => {
+    it("the icon contain this path hash value 82f5be4a5c07199cb75dacec50b90b2a", () => {
+      render(<Icon name="twitter" />);
+      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+        "82f5be4a5c07199cb75dacec50b90b2a"
+      );
+    });
+  });
+  describe("When a icon is created with name youtube", () => {
+    it("the icon contain this path hash value 43342876c2fc40e5b2afe621443ff95b", () => {
+      render(<Icon name="youtube" />);
+      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+        "43342876c2fc40e5b2afe621443ff95b"
+      );
+    });
+  });
+  describe("When a icon is created with name close", () => {
+    it("the icon contain this path hash value fe53fa5bf815b6d5983fcadf9a15d3d1", () => {
+      render(<Icon name="close" />);
+      expect(md5(screen.getByTestId("icon").getAttribute("d"))).toEqual(
+        "fe53fa5bf815b6d5983fcadf9a15d3d1"
+      );
+    });
+  });
+});
