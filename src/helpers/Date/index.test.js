07b3f7164a2b01d53f157e0e6d67b9389c927a79
TristanAly <tristan.aly@gmail.com>
2023-10-12 14:16:23 +0200

    Completed test date helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,18 @@
 /**
- * 
+ *
  */
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
+import { MONTHS } from ".";
 
+describe("Date helper", () => {
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      let date = new Date("2022-01-01T10:10:00Z");
+      expect(MONTHS[date.getMonth()]).toEqual(MONTHS[0]);
+    });
+    it("the function return juillet for 2022-07-08 as date", () => {
+      let date = new Date("2022-07-01T10:10:00Z");
+      expect(MONTHS[date.getMonth()]).toEqual(MONTHS[6]);
+    });
+  });
+});
